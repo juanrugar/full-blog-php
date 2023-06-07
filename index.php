a0f009f4499129eb8e86b981394d18fc88591810
Juanrugar <jruigar138@gmail.com>
2023-06-07 12:15:04 +0200

    Static part completed

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
+<!-- The first include should be config.php -->
 
-
-<?php require_once('includes/head_section.php') ?>
+<?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/head.php') ?>
+<title>LifeBlog | Landing Point</title>
 
 </head>
 <body>
 	<!-- container - wraps whole page -->
 	<div class="container">
 		<!-- navbar -->
-		<div class="navbar">
-			<div class="logo_div">
-				<a href="index.php"><h1>LifeBlog</h1></a>
-			</div>
-			<ul>
-			  <li><a class="active" href="index.php">Home</a></li>
-			  <li><a href="#news">News</a></li>
-			  <li><a href="#contact">Contact</a></li>
-			  <li><a href="#about">About</a></li>
-			</ul>
-		</div>
+		<?php include( ROOT_PATH . '/includes/navbar.php') ?>
 		<!-- // navbar -->
-
+		
+		<!-- banner -->
+		<?php include( ROOT_PATH . '/includes/banner.php') ?>
+		<!-- // banner -->
+		
 		<!-- Page content -->
 		<div class="content">
 			<h2 class="content-title">Recent Articles</h2>
@@ -29,9 +25,7 @@
 		<!-- // Page content -->
 
 		<!-- footer -->
-		<div class="footer">
-			<p>MyViewers &copy; <?php echo date('Y'); ?></p>
-		</div>
+		<?php include( ROOT_PATH . '/includes/footer.php') ?>
 		<!-- // footer -->
 
 	</div>
