8f79c4f89e9f942ddf2269f5618697e01ec16ff9
Juanrugar <jruigar138@gmail.com>
2023-06-09 13:23:47 +0200

    Front-end completed. Back-end begun.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php require_once('config.php') ?>
 <?php require_once( ROOT_PATH . '/includes/head.php') ?>
 <?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+<?php require_once( ROOT_PATH . '/includes/registration_login.php')?>
 
 <!-- Retrieve all posts from database -->
 <?php $posts = getPublishedPosts(); ?>
@@ -29,6 +30,14 @@
 			<?php foreach ($posts as $post): ?>
 				<div class="post" style="margin-left: 0px;">
 					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+					<!-- Added this if statement... -->
+					<?php if (isset($post['topic']['name'])): ?>
+					<a href="<?php echo BASE_URL . 'filtered_posts.php?topic=' . $post['topic']['id'] ?>"
+							class="btn category">
+						<?php echo $post['topic']['name'] ?>
+					</a>
+					<?php endif ?>
+
 					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
 						<div class="post_info">
 							<h3><?php echo $post['title'] ?></h3>
